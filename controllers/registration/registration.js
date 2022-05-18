8224712c880941e28474b1618e68346e50981b4c
m720 <37011075+m720@users.noreply.github.com>
2022-05-18 16:52:53 +0200

    upload to s3 added
    the register now uploads the CV to the S3 Bucket and returns the Link

--- controllers/registration/registration.js
+++ controllers/registration/registration.js
@@ -1,10 +1,23 @@
+const s3Mod = require('./s3');
+
     //load registration page
-exports.getRegistration =(req, res, next)=>{
+function getRegistration(req, res, next){
     res.render('./registration/registration');
 }
 
     //form submission handler
-exports.postRegistration =(req, res, next)=>{
+function postRegistration(req, res, next){
     const data = [req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q5];
     console.log(data);
+    new Promise(async(resolve, reject)=>{
+    const fileLink= await s3Mod.UploadToS3(req.UniqefileName);
+    // console.log(fileLink);
+    resolve(fileLink);
+    }).then((fileLink)=>{
+        console.log(fileLink);
+        res.send("sucessfull");
+    })
+
+
 }
+module.exports = {getRegistration, postRegistration}
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+require('dotenv').config()
 const app =express();
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
+//middelware for file handleing files
 
+const fileStorage = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, './utils/CVs');
+    },
+    filename: (req, file, cb)=>{
+        
+        UniqefileName =new Date().toISOString().replace(/:/g, ' ')+'_' + file.originalname;
+        req.UniqefileName = UniqefileName;
+        cb(null,UniqefileName);
+    }
+})
+app.use(multer({storage: fileStorage}).single("q6"));
 //setting up the views
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -14,6 +29,8 @@ const registrationRoutes =require('./routes/registration');
 const indexroutes = require('./routes/index');
 //to handle 404 errors
 const errorController =require('./controllers/error');
+const { file } = require('googleapis/build/src/apis/file');
+const res = require('express/lib/response');
 
 app.use('/register', registrationRoutes);
 app.use(indexroutes);
